41cfa86b83555b4f1a2a07737b8c0eefec8860b9
crismtzg <cmtzgrc@gmail.com>
2021-05-24 21:03:26 -0500

    creacion de mifooter

---
+++ cmp/mi-footer.js
@@ -0,0 +1,8 @@
+class MiFooter extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = /* html */
+        `Copyright &copy; 2021 Martinez Garcia Cristhian. (3CV50)`;
+    }
+  }
+  customElements.define("mi-footer", MiFooter);
+  
